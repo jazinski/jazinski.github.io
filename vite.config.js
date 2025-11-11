cb6d8db31b856911321ecc6c4ebc5e14c6f4d4ff
Christopher Jazinski <cjazinski@users.noreply.github.com>
2025-11-11 16:07:24 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
+// https://vitejs.dev/config/
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/www/',
-})
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const PUBLIC_URL = process.env.VITE_PUBLIC_URL || '/hr/portal/';
+
+  return defineConfig({
+    envPrefix: 'REACT_',
+    base: `${PUBLIC_URL}`,
+    plugins: [mkcert(), react()],
+    server: {
+      port: 3000,
+      https: true,
+    },
+  });
+};
